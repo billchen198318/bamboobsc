b7a55e6b2aef490a3ba026cf589ee459159348a1
Chen Xin Nien <chen.xin.nien@gmail.com>
2016-11-04 16:33:13 +0800

    Update code.

--- core-base/src/com/netsteadfast/greenstep/sys/WebMessagePublishListener.java
+++ core-base/src/com/netsteadfast/greenstep/sys/WebMessagePublishListener.java
@@ -45,100 +45,129 @@ import com.netsteadfast.greenstep.publish.BaseMessagePublishService;
 import com.netsteadfast.greenstep.service.ISysMsgNoticeConfigService;
 import com.netsteadfast.greenstep.vo.SysMsgNoticeConfigVO;
 
-@SuppressWarnings("unchecked")
 public class WebMessagePublishListener implements ServletContextListener {
 	private final static long SLEEP_TIME = 1000 * 5;
 	private ISysMsgNoticeConfigService<SysMsgNoticeConfigVO, TbSysMsgNoticeConfig, String> sysMsgNoticeConfigService;
 	private List<AsyncContext> asyncContexts = new ArrayList<AsyncContext>();
-	private Map<String, BaseMessagePublishService> publicServices = new HashMap<String, BaseMessagePublishService>();
-	private Map<String, SysMsgNoticeConfigVO> msgNoticeConfigMap = new HashMap<String, SysMsgNoticeConfigVO>();
+	private Thread webMessagePublishThread = null;
+	private WebMessagePublishRunnable webMessagePublishRunnable = null;
 	
+	public WebMessagePublishListener() {
+		
+	}
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		this.sysMsgNoticeConfigService = (ISysMsgNoticeConfigService<SysMsgNoticeConfigVO, TbSysMsgNoticeConfig, String>)
-				AppContext.getBean("core.service.SysMsgNoticeConfigService");								
+		if ( null == sysMsgNoticeConfigService ) {
+			this.sysMsgNoticeConfigService = (ISysMsgNoticeConfigService<SysMsgNoticeConfigVO, TbSysMsgNoticeConfig, String>)
+					AppContext.getBean("core.service.SysMsgNoticeConfigService");						
+		}		
 		event.getServletContext().setAttribute("asyncContexts", asyncContexts);
-		new Thread(new Runnable(){
-
-			@Override
-			public void run() {
+		this.webMessagePublishRunnable = new WebMessagePublishRunnable(this.asyncContexts);
+		this.webMessagePublishThread = new Thread(this.webMessagePublishRunnable);
+		this.webMessagePublishThread.start();		
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		this.webMessagePublishRunnable.stopRunning();
+		this.webMessagePublishThread = null;
+		this.webMessagePublishRunnable = null;
+	}	
+	
+	// --------------------------------------------------------------------------------------------------	
+	private class WebMessagePublishRunnable implements Runnable {
+		private volatile boolean running = true;
+		private Map<String, BaseMessagePublishService> publicServices = new HashMap<String, BaseMessagePublishService>();
+		private Map<String, SysMsgNoticeConfigVO> msgNoticeConfigMap = new HashMap<String, SysMsgNoticeConfigVO>();
+		private List<AsyncContext> asyncContexts = null;
+		
+		public WebMessagePublishRunnable(List<AsyncContext> asyncContexts) {
+			this.asyncContexts = asyncContexts;
+		}
+		
+		@Override
+		public void run() {
+			
+			while (this.running) {
 				
-				while (true) {					
-					synchronized (asyncContexts) {
-						for (AsyncContext context : asyncContexts) {
-							try {
-								WebMessagePublishBaseObj publishObj = null;
-								context.getResponse().setContentType("application/json; charset=" + Constants.BASE_ENCODING );
-								String id = StringUtils.defaultString(context.getRequest().getParameter("id")).trim();
-								String key = id + ":" + Constants.getSystem();
-								if (!StringUtils.isBlank(id)) {
-									SysMsgNoticeConfigVO config = msgNoticeConfigMap.get(key);
-									if (config==null) {
-										config = new SysMsgNoticeConfigVO();
-										config.setMsgId(id);
-										//config.setSystem(Constants.getSystem());
-										try {					
-											DefaultResult<SysMsgNoticeConfigVO> result = sysMsgNoticeConfigService.findByUK(config);
-											if (result.getValue()!=null) {
-												config = result.getValue();
-												msgNoticeConfigMap.put(key, config);
-											}
+				synchronized (asyncContexts) {
+					for (AsyncContext context : asyncContexts) {
+						try {
+							WebMessagePublishBaseObj publishObj = null;
+							context.getResponse().setContentType("application/json; charset=" + Constants.BASE_ENCODING );
+							String id = StringUtils.defaultString(context.getRequest().getParameter("id")).trim();
+							String key = id + ":" + Constants.getSystem();
+							if (!StringUtils.isBlank(id)) {
+								SysMsgNoticeConfigVO config = msgNoticeConfigMap.get(key);
+								if (config==null) {
+									config = new SysMsgNoticeConfigVO();
+									config.setMsgId(id);
+									//config.setSystem(Constants.getSystem());
+									try {					
+										DefaultResult<SysMsgNoticeConfigVO> result = sysMsgNoticeConfigService.findByUK(config);
+										if (result.getValue()!=null) {
+											config = result.getValue();
+											msgNoticeConfigMap.put(key, config);
+										}
+									} catch (ServiceException e) {
+										e.printStackTrace();
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
+								if (config!=null && !StringUtils.isBlank(config.getOid()) ) {
+									BaseMessagePublishService messagePublishService = publicServices.get(key);
+									if (messagePublishService==null) {
+										try {
+											messagePublishService = (BaseMessagePublishService)Class.forName(config.getClassName()).newInstance();
+											publicServices.put(key, messagePublishService);								
+										} catch (InstantiationException e) {
+											e.printStackTrace();
+										} catch (IllegalAccessException e) {
+											e.printStackTrace();
+										} catch (ClassNotFoundException e) {
+											e.printStackTrace();
+										}
+									}
+									if (messagePublishService!=null) { // 正確建立完成 BaseMessagePublishService  
+										try {
+											publishObj = messagePublishService.execute(config, context.getRequest());
 										} catch (ServiceException e) {
 											e.printStackTrace();
 										} catch (Exception e) {
 											e.printStackTrace();
-										}
+										}											
 									}
-									if (config!=null && !StringUtils.isBlank(config.getOid()) ) {
-										BaseMessagePublishService messagePublishService = publicServices.get(key);
-										if (messagePublishService==null) {
-											try {
-												messagePublishService = (BaseMessagePublishService)Class.forName(config.getClassName()).newInstance();
-												publicServices.put(key, messagePublishService);								
-											} catch (InstantiationException e) {
-												e.printStackTrace();
-											} catch (IllegalAccessException e) {
-												e.printStackTrace();
-											} catch (ClassNotFoundException e) {
-												e.printStackTrace();
-											}
-										}
-										if (messagePublishService!=null) { // 正確建立完成 BaseMessagePublishService  
-											try {
-												publishObj = messagePublishService.execute(config, context.getRequest());
-											} catch (ServiceException e) {
-												e.printStackTrace();
-											} catch (Exception e) {
-												e.printStackTrace();
-											}											
-										}
-									}								
-								}
-								if (publishObj==null) {
-									publishObj = new WebMessagePublishBaseObj();
-								}
-								context.getResponse().getWriter().println( JSONObject.fromObject(publishObj).toString() ); 
-								context.complete();
-							} catch (IOException e) {
-								e.printStackTrace();
+								}								
 							}
+							if (publishObj==null) {
+								publishObj = new WebMessagePublishBaseObj();
+							}
+							context.getResponse().getWriter().println( JSONObject.fromObject(publishObj).toString() ); 
+							context.complete();
+						} catch (IOException e) {
+							e.printStackTrace();
 						}
-						asyncContexts.clear();
 					}
-					try {
-						Thread.sleep(SLEEP_TIME);
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}					
+					asyncContexts.clear();
 				}
+				try {
+					Thread.sleep(SLEEP_TIME);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}		
+				
 			}
 			
-		}).start();		
+		}
+		
+	    public void stopRunning() {
+	        this.running = false;
+	    }
+		
 	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-
-	}
+	// --------------------------------------------------------------------------------------------------
 	
 }
