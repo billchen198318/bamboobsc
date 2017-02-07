078dd75cbf7a7e8d06930b5d8e4052c32aae3679
billchen198318 <chen.xin.nien@gmail.com>
2017-02-07 20:13:14 +0800

    Update

--- gsbsc-web/src/com/netsteadfast/greenstep/bsc/action/TsaSaveOrUpdateAction.java
+++ gsbsc-web/src/com/netsteadfast/greenstep/bsc/action/TsaSaveOrUpdateAction.java
@@ -26,6 +26,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts2.json.annotations.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +39,15 @@ import com.netsteadfast.greenstep.base.exception.ControllerException;
 import com.netsteadfast.greenstep.base.exception.ServiceException;
 import com.netsteadfast.greenstep.base.model.ControllerAuthority;
 import com.netsteadfast.greenstep.base.model.ControllerMethodAuthority;
+import com.netsteadfast.greenstep.base.model.DefaultResult;
+import com.netsteadfast.greenstep.bsc.action.utils.BscNumberFieldCheckUtils;
+import com.netsteadfast.greenstep.bsc.action.utils.NotBlankFieldCheckUtils;
+import com.netsteadfast.greenstep.bsc.model.BscMeasureDataFrequency;
 import com.netsteadfast.greenstep.bsc.service.logic.ITsaLogicService;
+import com.netsteadfast.greenstep.util.SimpleUtils;
+import com.netsteadfast.greenstep.vo.TsaMaCoefficientsVO;
+import com.netsteadfast.greenstep.vo.TsaMeasureFreqVO;
+import com.netsteadfast.greenstep.vo.TsaVO;
 
 @ControllerAuthority(check=true)
 @Controller("bsc.web.controller.TsaSaveOrUpdateAction")
@@ -64,17 +73,147 @@ public class TsaSaveOrUpdateAction extends BaseJsonAction {
 	public void setTsaLogicService(ITsaLogicService tsaLogicService) {
 		this.tsaLogicService = tsaLogicService;
 	}
-
-	private void save() throws ControllerException, AuthorityException, ServiceException, Exception {
+	
+	private void checkFields() throws ControllerException {
+		this.getCheckFieldHandler()
+		.add("name", NotBlankFieldCheckUtils.class, "Name is required!")
+		.add("integration", BscNumberFieldCheckUtils.class, "Integration field value must be number!")
+		.add("forecast", BscNumberFieldCheckUtils.class, "Forecast field value must be number!")
+		.add("coefficient1", BscNumberFieldCheckUtils.class, "Coefficient (1) field value must be number!")
+		.add("coefficient2", BscNumberFieldCheckUtils.class, "Coefficient (2) field value must be number!")
+		.add("coefficient3", BscNumberFieldCheckUtils.class, "Coefficient (3) field value must be number!")
+		.process().throwMessage();
 		
+		this.checkMeasureFrequency();
 	}
 	
-	private void update() throws ControllerException, AuthorityException, ServiceException, Exception {
+	private void checkMeasureFrequency() throws ControllerException {
+		String frequency = this.getFields().get("measureFreq_frequency");
+		String startDate = this.getFields().get("measureFreq_startDate");
+		String endDate = this.getFields().get("measureFreq_endDate");
+		String startYearDate = this.getFields().get("measureFreq_startYearDate");
+		String endYearDate = this.getFields().get("measureFreq_endYearDate");
+		if ( BscMeasureDataFrequency.FREQUENCY_DAY.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_WEEK.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_MONTH.equals(frequency) ) {
+			if ( StringUtils.isBlank( startDate ) || StringUtils.isBlank( endDate ) ) {
+				super.throwMessage("measureFreq_startDate|measureFreq_endDate", "Measure settings, Start-date and end-date is required!");		
+			}
+		}
+		if ( BscMeasureDataFrequency.FREQUENCY_QUARTER.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_HALF_OF_YEAR.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_YEAR.equals(frequency) ) {
+			if ( StringUtils.isBlank( startYearDate ) || StringUtils.isBlank( endYearDate ) ) {
+				super.throwMessage("measureFreq_startYearDate|measureFreq_endYearDate", "Measure settings, Start-year and end-year is required!");			
+			}			
+		}		
+		if ( !StringUtils.isBlank( startDate ) || !StringUtils.isBlank( endDate ) ) {
+			if ( !SimpleUtils.isDate( startDate ) ) {
+				super.throwMessage("measureFreq_startDate", "Measure settings, Start-date format is incorrect!");
+			}
+			if ( !SimpleUtils.isDate( endDate ) ) {
+				super.throwMessage("measureFreq_endDate", "Measure settings, End-date format is incorrect!");		
+			}
+			if ( Integer.parseInt( endDate.replaceAll("/", "").replaceAll("-", "") )
+					< Integer.parseInt( startDate.replaceAll("/", "").replaceAll("-", "") ) ) {
+				super.throwMessage("measureFreq_startDate|measureFreq_endDate", "Measure settings, Start-date / end-date incorrect!");
+			}			
+		}
+		if ( !StringUtils.isBlank( startYearDate ) && !StringUtils.isBlank( endYearDate ) ) {
+			if ( !SimpleUtils.isDate( startYearDate+"/01/01" ) ) {
+				super.throwMessage("measureFreq_startYearDate", "Measure settings, Start-year format is incorrect!");			
+			}
+			if ( !SimpleUtils.isDate( endYearDate+"/01/01" ) ) {
+				super.throwMessage("measureFreq_endYearDate", "Measure settings, End-year format is incorrect!");				
+			}
+			if ( Integer.parseInt( endYearDate.replaceAll("/", "").replaceAll("-", "") )
+					< Integer.parseInt( startYearDate.replaceAll("/", "").replaceAll("-", "") ) ) {
+				super.throwMessage("measureFreq_startYearDate|measureFreq_endYearDate", "Measure settings, Start-year / end-year incorrect!");	
+			}					
+		}
+		String dataFor = this.getFields().get("measureFreq_dataFor");
+		if ("organization".equals(dataFor) 
+				&& this.isNoSelectId(this.getFields().get("measureFreq_measureDataOrganizationOid")) ) {
+			super.throwMessage("measureFreq_measureDataOrganizationOid", "Measure settings, Please select measure-data organization!");
+		}
+		if ("employee".equals(dataFor)
+				&& this.isNoSelectId(this.getFields().get("measureFreq_measureDataEmployeeOid")) ) {
+			super.throwMessage("measureFreq_measureDataEmployeeOid", "Measure settings, Please select measure-data employee!");
+		}			
+	}
+
+	private void saveOrUpdate(String type) throws ControllerException, AuthorityException, ServiceException, Exception {
+		this.checkFields();
+		TsaVO tsa = new TsaVO();
+		this.transformFields2ValueObject(tsa, new String[]{"name", "integration", "forecast", "description"});
+		this.getFields().remove("measureFreq_date1");
+		this.getFields().remove("measureFreq_date2");
+		this.getFields().remove("measureFreq_dateType");
+		String frequency = this.getFields().get("measureFreq_frequency");		
+		if (BscMeasureDataFrequency.FREQUENCY_DAY.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_WEEK.equals(frequency) 
+				|| BscMeasureDataFrequency.FREQUENCY_MONTH.equals(frequency) ) {
+			this.getFields().put("measureFreq_date1", this.getFields().get("measureFreq_startDate"));
+			this.getFields().put("measureFreq_date2", this.getFields().get("measureFreq_endDate"));
+		} else {
+			this.getFields().put("measureFreq_date1", this.getFields().get("measureFreq_startYearDate"));
+			this.getFields().put("measureFreq_date2", this.getFields().get("measureFreq_endYearDate"));
+		}
+		String dataFor = this.getFields().get("measureFreq_dataFor");
+		this.getFields().put("measureFreq_dateType", "3");
+		if ("organization".equals(dataFor)) {
+			this.getFields().put("measureFreq_dateType", "1");
+		}
+		if ("employee".equals(dataFor)) {
+			this.getFields().put("measureFreq_dateType", "2");
+		}
+		TsaMeasureFreqVO measureFreq = new TsaMeasureFreqVO();
+		this.transformFields2ValueObject(
+				measureFreq, 
+				new String[]{
+						"freq",
+						"dataType",
+						"organizationOid",
+						"employeeOid",
+						"startDate",
+						"endDate"
+				}, 
+				new String[]{
+						"measureFreq_frequency",
+						"measureFreq_dateType",
+						"measureFreq_measureDataOrganizationOid",
+						"measureFreq_measureDataEmployeeOid",
+						"measureFreq_date1",
+						"measureFreq_date2"
+				});
+		TsaMaCoefficientsVO coefficient1 = new TsaMaCoefficientsVO();
+		TsaMaCoefficientsVO coefficient2 = new TsaMaCoefficientsVO();
+		TsaMaCoefficientsVO coefficient3 = new TsaMaCoefficientsVO();
+		this.transformFields2ValueObject(coefficient1, new String[]{"coefficient1"});
+		this.transformFields2ValueObject(coefficient2, new String[]{"coefficient2"});
+		this.transformFields2ValueObject(coefficient3, new String[]{"coefficient3"});
+		
+		DefaultResult<TsaVO> result = null;
+		if ("save".equals(type)) {
+			result = this.tsaLogicService.create(tsa, measureFreq, coefficient1, coefficient2, coefficient3);
+		} else {
+			result = this.tsaLogicService.update(tsa, measureFreq, coefficient1, coefficient2, coefficient3);
+		}
+		this.message = result.getSystemMessage().getValue();
+		if (result.getValue()!=null) {
+			this.success = IS_YES;
+		}
 		
 	}
 	
 	private void delete() throws ControllerException, AuthorityException, ServiceException, Exception {
-		
+		TsaVO tsa = new TsaVO();
+		this.transformFields2ValueObject(tsa, new String[]{"oid"});
+		DefaultResult<Boolean> result = this.tsaLogicService.delete(tsa);
+		this.message = result.getSystemMessage().getValue();
+		if (result.getValue()!=null && result.getValue()) {
+			this.success = IS_YES;
+		}		
 	}
 	
 	/**
@@ -90,7 +229,7 @@ public class TsaSaveOrUpdateAction extends BaseJsonAction {
 				this.message = this.getNoAllowMessage();
 				return SUCCESS;
 			}
-			this.save();
+			this.saveOrUpdate("save");
 		} catch (ControllerException ce) {
 			this.message=ce.getMessage().toString();
 		} catch (AuthorityException ae) {
@@ -119,7 +258,7 @@ public class TsaSaveOrUpdateAction extends BaseJsonAction {
 				this.message = this.getNoAllowMessage();
 				return SUCCESS;
 			}
-			this.update();
+			this.saveOrUpdate("update");
 		} catch (ControllerException ce) {
 			this.message=ce.getMessage().toString();
 		} catch (AuthorityException ae) {
