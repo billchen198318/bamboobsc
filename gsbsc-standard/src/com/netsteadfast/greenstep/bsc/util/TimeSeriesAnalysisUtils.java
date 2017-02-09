d9a67b58cf7bb6e8daa5f53b782a5760b22c6e12
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-02-09 17:12:57 +0800

    Update

--- gsbsc-standard/src/com/netsteadfast/greenstep/bsc/util/TimeSeriesAnalysisUtils.java
+++ gsbsc-standard/src/com/netsteadfast/greenstep/bsc/util/TimeSeriesAnalysisUtils.java
@@ -22,14 +22,21 @@
 package com.netsteadfast.greenstep.bsc.util;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.chain.Context;
+import org.apache.commons.lang3.StringUtils;
 import org.espy.arima.ArimaForecaster;
 import org.espy.arima.DefaultArimaForecaster;
 import org.espy.arima.DefaultArimaProcess;
 
 import com.netsteadfast.greenstep.base.AppContext;
+import com.netsteadfast.greenstep.base.SysMessageUtil;
+import com.netsteadfast.greenstep.base.exception.ServiceException;
+import com.netsteadfast.greenstep.base.model.DefaultResult;
+import com.netsteadfast.greenstep.base.model.GreenStepSysMsgConstants;
 import com.netsteadfast.greenstep.bsc.service.ITsaMaCoefficientsService;
 import com.netsteadfast.greenstep.bsc.service.ITsaService;
 import com.netsteadfast.greenstep.po.hbm.BbTsa;
@@ -48,9 +55,23 @@ public class TimeSeriesAnalysisUtils {
 		tsaMaCoefficientsService = (ITsaMaCoefficientsService<TsaMaCoefficientsVO, BbTsaMaCoefficients, String>) AppContext.getBean("bsc.service.TsaMaCoefficientsService");
 	}
 	
-	public static double[] getForecastNext(TsaVO tsa, double[] observations) throws Exception {
+	public static TsaVO getParam(String tsaOid) throws ServiceException, Exception {
+		if (StringUtils.isBlank(tsaOid)) {
+			throw new ServiceException(SysMessageUtil.get(GreenStepSysMsgConstants.PARAMS_BLANK));
+		}
+		TsaVO tsa = new TsaVO();
+		tsa.setOid(tsaOid);
+		DefaultResult<TsaVO> result = tsaService.findObjectByOid(tsa);
+		if (result.getValue() == null) {
+			throw new ServiceException( result.getSystemMessage().getValue() );
+		}
+		tsa = result.getValue();
+		return tsa;
+	}
+	
+	public static double[] getForecastNext(TsaVO tsa, double[] observations) throws ServiceException, Exception {
 		if (null == observations || observations.length < 1) {
-			throw new IllegalArgumentException("observations array cannot not zero");
+			throw new IllegalArgumentException("observations array cannot zero size");
 		}
 		Map<String, Object> paramMap = new HashMap<String, Object>();
 		paramMap.put("tsaOid", tsa.getOid());
@@ -61,14 +82,21 @@ public class TimeSeriesAnalysisUtils {
 			throw new IllegalStateException("Ma coefficients data error");
 		}
 		
-        DefaultArimaProcess arimaProcess = new DefaultArimaProcess();
-        arimaProcess.setMaCoefficients(coefficients.get(0).getSeqValue(), coefficients.get(1).getSeqValue(), coefficients.get(2).getSeqValue());
-        arimaProcess.setIntegrationOrder( tsa.getIntegrationOrder() );
-        
-        ArimaForecaster arimaForecaster = new DefaultArimaForecaster(arimaProcess, observations);
-        double[] forecast = arimaForecaster.next( tsa.getForecastNext() );
-        
-        return forecast;
+		DefaultArimaProcess arimaProcess = new DefaultArimaProcess();
+		arimaProcess.setMaCoefficients(coefficients.get(0).getSeqValue(), coefficients.get(1).getSeqValue(), coefficients.get(2).getSeqValue());
+		arimaProcess.setIntegrationOrder( tsa.getIntegrationOrder() );
+		
+		ArimaForecaster arimaForecaster = new DefaultArimaForecaster(arimaProcess, observations);
+		double[] forecast = arimaForecaster.next( tsa.getForecastNext() );
+		
+		return forecast;
 	}
+	
+//	public static VisionVO getResult(String tsaOid, Context queryContext) throws ServiceException, Exception {
+//		
+//		throw new java.lang.UnsupportedOperationException("not work");
+//		//return null;
+//		
+//	}
 	
 }
