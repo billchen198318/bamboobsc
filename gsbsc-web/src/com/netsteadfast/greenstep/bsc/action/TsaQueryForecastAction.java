298baac28d028f29985ce6708cde2fbe87a29750
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-02-09 12:21:07 +0800

    Update

---
+++ gsbsc-web/src/com/netsteadfast/greenstep/bsc/action/TsaQueryForecastAction.java
@@ -0,0 +1,125 @@
+/* 
+ * Copyright 2012-2017 bambooCORE, greenstep of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package com.netsteadfast.greenstep.bsc.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.json.annotations.JSON;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.netsteadfast.greenstep.base.action.BaseJsonAction;
+import com.netsteadfast.greenstep.base.exception.AuthorityException;
+import com.netsteadfast.greenstep.base.exception.ControllerException;
+import com.netsteadfast.greenstep.base.exception.ServiceException;
+import com.netsteadfast.greenstep.base.model.ControllerAuthority;
+import com.netsteadfast.greenstep.base.model.ControllerMethodAuthority;
+
+@ControllerAuthority(check=true)
+@Controller("bsc.web.controller.TsaQueryForecastAction")
+@Scope
+public class TsaQueryForecastAction extends BaseJsonAction {
+	private static final long serialVersionUID = -449705564005894371L;
+	protected Logger logger = Logger.getLogger(TsaQueryForecastAction.class);
+	
+	private String message = "";
+	private String success = IS_NO;	
+	
+	public TsaQueryForecastAction() {
+		super();
+	}
+	
+	private void getContent() throws ControllerException, AuthorityException, ServiceException, Exception {
+		
+	}
+	
+	/**
+	 * bsc.tsaQueryForecastAction.action
+	 */
+	@JSON(serialize=false)
+	@ControllerMethodAuthority(programId="BSC_PROG007D0002Q")
+	public String execute() throws Exception {
+		try {
+			if (!this.allowJob()) {
+				this.message = this.getNoAllowMessage();
+				return SUCCESS;
+			}
+			this.getContent();
+		} catch (ControllerException ce) {
+			this.message=ce.getMessage().toString();
+		} catch (AuthorityException ae) {
+			this.message=ae.getMessage().toString();
+		} catch (ServiceException se) {
+			this.message=se.getMessage().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (e.getMessage()==null) { 
+				this.message=e.toString();
+				this.logger.error(e.toString());
+			} else {
+				this.message=e.getMessage().toString();
+				this.logger.error(e.getMessage());
+			}						
+			this.success = IS_EXCEPTION;
+		}
+		return SUCCESS;		
+	}
+
+	@JSON
+	@Override
+	public String getLogin() {
+		return super.isAccountLogin();
+	}
+	
+	@JSON
+	@Override
+	public String getIsAuthorize() {
+		return super.isActionAuthorize();
+	}	
+
+	@JSON
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+
+	@JSON
+	@Override
+	public String getSuccess() {
+		return this.success;
+	}
+
+	@JSON
+	@Override
+	public List<String> getFieldsId() {
+		return this.fieldsId;
+	}
+	
+	@JSON
+	@Override
+	public Map<String, String> getFieldsMessage() {
+		return this.fieldsMessage;
+	}
+	
+}
