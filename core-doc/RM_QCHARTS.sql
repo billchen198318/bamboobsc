778627a0f37c22c3be2fdceda0f9264fe5bf2ae7
Bill chen <chen.xin.nien@gmail.com>
2018-02-28 08:55:55 +0800

    Update

--- core-doc/RM_QCHARTS.sql
+++ core-doc/RM_QCHARTS.sql
@@ -20,6 +20,8 @@ delete from tb_sys_ws_config where SYSTEM='QCHARTS';
 
 delete from tb_sys_ctx_bean where SYSTEM='QCHARTS';
 
+SET foreign_key_checks = 0;
+
 DROP TABLE `qc_data_query`;
 DROP TABLE `qc_data_query_mapper`;
 DROP TABLE `qc_data_query_mapper_set`;
@@ -28,3 +30,16 @@ DROP TABLE `qc_data_source_driver`;
 DROP TABLE `qc_olap_catalog`;
 DROP TABLE `qc_olap_conf`;
 DROP TABLE `qc_olap_mdx`;
+DROP TABLE `qrtz_z_qcharts_blob_triggers`;
+DROP TABLE `qrtz_z_qcharts_calendars`;
+DROP TABLE `qrtz_z_qcharts_cron_triggers`;
+DROP TABLE `qrtz_z_qcharts_fired_triggers`;
+DROP TABLE `qrtz_z_qcharts_job_details`;
+DROP TABLE `qrtz_z_qcharts_locks`;
+DROP TABLE `qrtz_z_qcharts_paused_trigger_grps`;
+DROP TABLE `qrtz_z_qcharts_scheduler_state`;
+DROP TABLE `qrtz_z_qcharts_simple_triggers`;
+DROP TABLE `qrtz_z_qcharts_simprop_triggers`;
+DROP TABLE `qrtz_z_qcharts_triggers`;
+
+SET foreign_key_checks = 1;
